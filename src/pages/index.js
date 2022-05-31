72bf3a6bef5b422b786195f5ef34b557f442f498
petethomasmusique <petethomasmusique@gmail.com>
2022-05-31 17:14:08 +0100

    fix broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className={clsx("button button--secondary button--lg", styles.button)}
-            to="/docs/intro">
+            to="/docs/tutorial/intro">
             Zen Tutorial - 5min
           </Link>
         </div>
