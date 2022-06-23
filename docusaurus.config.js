c2ab1a71d574a7e0ee868e1812f97de81f545315
petethomasmusique <petethomasmusique@gmail.com>
2022-06-23 10:30:35 +0100

    add algolia info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,6 +40,29 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'U3FQLK6KYX',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '30af5ccc03e1b8beb6fe87a4ec20a6e9',
+  
+        indexName: 'cephasteom',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
       navbar: {
         title: 'Zen',
         logo: {
